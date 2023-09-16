ef6e574cb803b20963c7c26f7217d3c293b93a2b
KingKingsta <45103249+KingKingsta@users.noreply.github.com>
2023-09-16 12:48:01 -0400

    appliance5.2.1

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -1,65 +1,70 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-import { Button } from '@/components/Button'
-import { SelectField, TextArea, TextField } from '@/components/Fields'
-import { Logo } from '@/components/Logos'
-import { SlimLayout } from '@/components/SlimLayout'
-import { type Metadata } from 'next'
+import { Button } from '@/components/Button';
+import { SelectField, TextArea, TextField } from '@/components/Fields';
+import { LogoMedium } from '@/components/Logos';
+import { SlimLayout } from '@/components/SlimLayout';
+import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Sign Up',
-}
+    title: 'Sign Up',
+};
 
 export default function Register() {
-  return (
-    <SlimLayout>
-      <Link href={"/"} aria-label="Back">
-        <span>
-          <span aria-hidden="true">&larr;</span> Back
-        </span>
-      </Link>
-      <div className="flex">
-        <Link href="/" aria-label="Home">
-          <Logo className="h-10 w-auto" />
-        </Link>
-      </div>
-      <h2 className="mt-10 text-lg font-semibold text-gray-900">
-        Create account
-      </h2>
-      <form
-        action="#"
-        className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
-      >
-        <TextField
-          label="First name"
-          name="first_name"
-          type="text"
-          autoComplete="given-name"
-          required
-        />
-        <TextField
-          label="Last name"
-          name="last_name"
-          type="text"
-          autoComplete="family-name"
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Email address"
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-        />
-        <div className="col-span-full">
-          <Button type="submit" variant="solid" color="green" className="w-full">
-            <span>
-              Create <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Button>
-        </div>
-      </form>
-    </SlimLayout>
-  )
+    return (
+        <SlimLayout>
+            <Link href={'/'} aria-label="Back">
+                <span>
+                    <span aria-hidden="true">&larr;</span> Back
+                </span>
+            </Link>
+            <div className="flex">
+                <Link href="/" aria-label="Home">
+                    <LogoMedium className="h-10 w-auto" />
+                </Link>
+            </div>
+            <h2 className="mt-10 text-lg font-semibold text-gray-900">
+                Create account
+            </h2>
+            <form
+                action="#"
+                className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
+            >
+                <TextField
+                    label="First name"
+                    name="first_name"
+                    type="text"
+                    autoComplete="given-name"
+                    required
+                />
+                <TextField
+                    label="Last name"
+                    name="last_name"
+                    type="text"
+                    autoComplete="family-name"
+                    required
+                />
+                <TextField
+                    className="col-span-full"
+                    label="Email address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                />
+                <div className="col-span-full">
+                    <Button
+                        type="submit"
+                        variant="solid"
+                        color="green"
+                        className="w-full"
+                    >
+                        <span>
+                            Create <span aria-hidden="true">&rarr;</span>
+                        </span>
+                    </Button>
+                </div>
+            </form>
+        </SlimLayout>
+    );
 }
